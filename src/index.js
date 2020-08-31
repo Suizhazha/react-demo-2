ae2fe9d196198f103f46a3738f1d3f3e58ef5140
Suizhazha <573505403@qq.com>
2020-08-31 16:17:04 +0800

    使用内部数据state修改数据

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './style.css'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App(){
+  return (
+    <div className="App">
+      爸爸
+    <Son/>
+    </div>
+  )
+}
+
+class Son extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      n:0
+    }
+  }
+  add(){
+
+    // this.state.n+=1  Vue方法
+
+    // this.setState({ n: this.state.n + 1 });
+    // console.log(this.state.n)//n+1之前的数值
+
+    this.setState((state)=>{
+      return {n: state.n+1}
+    })
+  }
+  render() {
+    return (
+      <div className='Son'>
+        儿子 n:{this.state.n}
+        <button onClick={()=>this.add()}>+1</button>
+        <Grandson />
+      </div>
+    )
+  }
+}
+
+const Grandson = ()=>{
+  //const array = React.useState(0)
+  //const n = array[0]
+  //const setN = array[1]
+
+  const [n,setN] = React.useState(0)
+  return (
+    <div className='Grandson'>
+      孙子 n:{n}
+      <button onClick={()=>setN(n+1)}>+1</button>
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />,rootElement)
